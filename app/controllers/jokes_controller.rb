5cceadc25cc7f3b3cb8ccc38e102d42d2eda06f4
Caroline <carolinesalibc@gmail.com>
2024-08-15 16:48:43 -0600

    Add custom metric

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'prometheus_exporter/client'
 
 class JokesController < ApplicationController
   def index
@@ -6,14 +7,19 @@ class JokesController < ApplicationController
     joke_response = JSON.parse(Net::HTTP.get(uri))
 
     # Use it to test status code metric
-    if rand < 0.5
-      raise "A random error occurred!"
+    if rand < 0.2
+      # Simulate fake API timeout of 2 seconds
+      sleep(2)
+
+      raise "Error fetching fake API!"
     end
 
-    # Use it to test duration metric
-    # sleep(2)
+    @joke_title = joke_response["setup"]
+    @joke = joke_response["delivery"] || joke_response["joke"]
 
-    @joke_setup = joke_response["setup"]
-    @joke_delivery = joke_response["delivery"] || joke_response["joke"]
+    # Custom metric
+    client = PrometheusExporter::Client.default
+    counter = client.register(:counter, "jokes_custom_metric", "whether a joke has a title")
+    counter.observe(1, has_title: @joke_title.present?)
   end
 end
